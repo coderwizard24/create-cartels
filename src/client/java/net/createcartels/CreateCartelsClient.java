763eb8d4d40acadee92ce84e809c14ce673bda31
Alan <sudoname2424@gmail.com>
2026-03-13 17:48:41 -0400

    Initial commit

---
+++ src/client/java/net/createcartels/CreateCartelsClient.java
@@ -0,0 +1,10 @@
+package net.createcartels;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CreateCartelsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
